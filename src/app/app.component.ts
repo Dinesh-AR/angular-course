94079c82cb5211847bf1be76ddc548443dd7349d
Dinesh AR <dinesh.msrit@gmail.com>
2023-05-04 07:12:29 +0530

    Manual Dependency Injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,40 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Component, Inject, InjectionToken, OnInit } from "@angular/core";
+import { Course } from "./model/course";
+import { Observable } from "rxjs";
+import { CoursesService } from "./services/courses.service";
 
+function courseServiceProvider(http: HttpClient): CoursesService {
+  return new CoursesService(http);
+}
+
+const COURSES_SERVICE = new InjectionToken<CoursesService>("COURSES_SERVICE");
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [
+    {
+      provide: COURSES_SERVICE,
+      useFactory: courseServiceProvider,
+      deps: [HttpClient],
+    },
+  ],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
+  courses;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(
+    @Inject(COURSES_SERVICE) private coursesService: CoursesService
+  ) {}
 
   ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
   }
-
-
-
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("course saved"));
+  }
 }
